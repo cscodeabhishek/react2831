4a664001da6b0c9bc19e764ee2a056043c8a8472
your_name <dev.codescse@gmail.com>
2024-11-05 15:48:19 +0530

    reactSTART

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // console.log("hello world");
 const container = document.getElementById("root");
-console.log(container);
+function Helloworld(){
+    return <h1>Hello Wordl</h1>;
+}
+// console.log(container);
 // type ,props,children
-const el = React.createElement("h1",null,"Hello reactJS")
-ReactDOM.createRoot(container).render(el);
+// const el = React.createElement("h1",null,"Hello reactJS")
+
+ReactDOM.createRoot(container).render(<Helloworld />);
