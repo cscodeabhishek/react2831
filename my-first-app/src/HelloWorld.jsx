bbd06df059bb8c604c410b823694a45cdd9ebd23
your_name <dev.codescse@gmail.com>
2024-11-06 17:17:25 +0530

    CRA

---
+++ my-first-app/src/HelloWorld.jsx
@@ -0,0 +1,9 @@
+function HelloWorld(){
+    return (
+        <>
+            <h1>This is Heading 1</h1>
+            <p>paragraph </p>
+        </>
+    );
+}
+export default HelloWorld;
--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom/client'; // client here is for 18 version
-function Hello(){
-  return <h1>Hello World</h1>
-}
-ReactDOM.createRoot(document.getElementById('root')).render(<Hello/>)
+import HelloWorld from './HelloWorld';
+const root =ReactDOM.createRoot(document.getElementById('root'));
+// create a simple component here
+// function HelloWorld(){
+//   return <h1>Hello World</h1>; // allowed to return only single element here ((multiple element returning is not allowed here))
+// }
+
+// to return multiple element you can use [reactFragment] 
+// short hand for writing the react Fragment is [ <> </> ]
+// function HelloWorld(){
+//   return <>
+//     <h1>Hello World</h1>
+//     <p>this is para</p>
+//     </>
+// }
+root.render(
+  <React.StrictMode>
+    <HelloWorld/>
+  </React.StrictMode>
+)
